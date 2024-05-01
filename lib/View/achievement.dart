11a9fae5bf955eecfc6b5e8476b4558a98cf464b
Hayato <hayato.314.nico@gmail.com>
2024-05-02 00:16:15 +0900

    Firebaseと連携

---
+++ lib/View/achievement.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AchievementWidget {
+  static Widget createWidget() {
+    return Center(
+      child: Text('これはアチーブメント'),
+    );
+  }
+}
---
+++ lib/View/archive.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ArchiveWidget {
+  static Widget createWidget() {
+    return Center(
+      child: Text('これはアーカイブ'),
+    );
+  }
+}
