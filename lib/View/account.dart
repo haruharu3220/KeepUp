f67ccf715f4078521a631cd918a9ba59f9dd07dc
Hayato <hayato.314.nico@gmail.com>
2024-05-01 22:28:50 +0900

    フッダーによる画面切り替えをFactory化

---
+++ lib/View/account.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class AccountWidget {
+  static Widget createWidget() {
+    return Center(
+      child: Text('これはAccount'),
+    );
+  }
+}
+
+
--- lib/View/test.dart
+++ lib/View/test.dart
@@ -1,10 +1,30 @@
-
 import 'package:flutter/cupertino.dart';
+
+import 'account.dart';
 
 class TestWidget{
   static Widget createWidget(int a){
-    return Center(
-      child: Text('_selectedIndex＝${a}'),
-    );
+
+    if(a == 0){
+      // return Center(
+      //   child: Text('_selectedIndex＝${a}!'
+      //       ),
+      // );
+      return AccountWidget.createWidget();
+    }
+
+    if(a == 1){
+      return Center(
+        child: Text('_selectedIndex＝${a}'),
+      );
+    }
+
+    if(a == 2){
+      return Center(
+        child: Text('_selectedIndex＝${a}'),
+      );
+    }
+
+    return Container();
   }
 }
